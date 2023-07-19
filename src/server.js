06acbded7ea32d349f7d00d2fe7c6a9df0d843db
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-07-18 22:57:31 -0300

    criando middleware em json

--- src/server.js
+++ src/server.js
@@ -1,24 +1,15 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 const users = [];
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk)
-  }
   
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch (error) {
-    request.body = null
-  }
+  await json(request, response);
 
   if (method === 'GET' && url === '/users') {
     return response
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users));
   }
 
