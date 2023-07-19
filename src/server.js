fdbb7c217c637e16567332f88c2e73bfed5791a7
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-07-18 22:47:18 -0300

    corpo da requisição em JSON

--- src/server.js
+++ src/server.js
@@ -2,20 +2,33 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((request, response) => {
-  const {method, url} = request;
+const server = http.createServer(async (request, response) => {
+  const { method, url } = request;
+  const buffers = [];
 
-  if(method === 'GET' && url === '/users') {
-    return response
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users));
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+  
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    request.body = null
   }
 
-  if(method === 'POST' && url === '/users'){
+  if (method === 'GET' && url === '/users') {
+    return response
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = request.body;
+
     users.push({
       id: 1,
-      name: 'Jonh doe',
-      email: 'jonh@email.com'
+      name,
+      email,
     });
 
     return response.writeHead(201).end()
