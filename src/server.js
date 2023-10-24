0ceba4d7cbb593c1fe7d0a0d03c533401c49b278
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-10-23 23:32:46 -0300

    separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
-import { Database } from './database.js';
 
 const users = [];
 
@@ -11,28 +10,17 @@ const server = http.createServer(async (request, response) => {
   
   await json(request, response);
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = route.find(route => {
+    return route.method === method && route.path === url
+  });
 
-    return response.end(JSON.stringify(users));
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = request.body;
-
-    const user = ({
-      id: 1,
-      name,
-      email,
-    });
-
-    database.insert('users', user)
-
-    return response.writeHead(201).end()
+  if(route){
+    return route.handler(request, response);
   }
 
   return response.writeHead(404).end('Not Found');
-});
+}
+);
 
 server.listen(3333);
 
